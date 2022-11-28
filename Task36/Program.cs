f1ba3e8895a03962ebcc598bf9590db331959e54
Tatiana443 <tvb399@yandex.ru>
2022-11-28 15:36:31 +0600

    Project Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+int[] array = CreateArrayRndInt(10, 1, 50);
+PrintArray(array);
+int SumElemOddPosition(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+        if (i % 2 != 0) sum += arr[i];
+    return sum;
+}
+int sumElemOddPosition = SumElemOddPosition(array);
+Console.WriteLine($" Сумма элементов, стоящих на нечетных позициях = {sumElemOddPosition}");
